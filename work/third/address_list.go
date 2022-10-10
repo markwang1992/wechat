077318e0c80b977c5aed168a7f6ed096d8c2ac8b
wang.yu <wangyu@uniondrug.com>
2022-10-10 17:37:12 +0800

    debug

--- work/third/address_list.go
+++ work/third/address_list.go
@@ -13,6 +13,8 @@ const (
 	DepartmentSimpleListURL = "https://qyapi.weixin.qq.com/cgi-bin/department/simplelist?access_token=%s&id=%d"
 	// UserSimpleListURL 获取部门成员
 	UserSimpleListURL = "https://qyapi.weixin.qq.com/cgi-bin/user/simplelist?access_token=%s&department_id=%d"
+	// UserListIdURL 获取成员ID列表
+	UserListIdURL = "https://qyapi.weixin.qq.com/cgi-bin/user/list_id?access_token=%s"
 )
 
 type (
@@ -73,4 +75,40 @@ func (r *Client) UserSimpleList(departmentId int) ([]*UserList, error) {
 		return nil, err
 	}
 	return result.UserList, nil
+}
+
+// UserListIdRequest 获取成员ID列表请求
+type UserListIdRequest struct {
+	Cursor string `json:"cursor"`
+	Limit  int    `json:"limit"`
+}
+
+// UserListIdResponse 获取成员ID列表响应
+type UserListIdResponse struct {
+	util.CommonError
+	NextCursor string      `json:"next_cursor"`
+	DeptUser   []*DeptUser `json:"dept_user"`
+}
+
+// DeptUser 用户-部门关系
+type DeptUser struct {
+	UserID     string `json:"userid"`
+	Department int    `json:"department"`
+}
+
+// UserListId 获取成员ID列表
+// see https://developer.work.weixin.qq.com/document/path/96067
+func (r *Client) UserListId(req *UserListIdRequest) (*UserListIdResponse, error) {
+	var (
+		err      error
+		response []byte
+	)
+	if response, err = util.PostJSON(fmt.Sprintf(UserListIdURL, r.AccessToken), req); err != nil {
+		return nil, err
+	}
+	result := &UserListIdResponse{}
+	if err = util.DecodeWithError(response, result, "UserListId"); err != nil {
+		return nil, err
+	}
+	return result, nil
 }
